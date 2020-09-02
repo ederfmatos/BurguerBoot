cf5d75918736ecebd6a35768bba4a693e0adb923
ederfmatos <ederfmatos@gmail.com>
2020-09-01 22:47:21 -0300

    Adicionado funções para envio de mensagem

--- js/index.js
+++ js/index.js
@@ -1,21 +1,47 @@
-const $ = document.querySelector.bind(document)
-const sendButton = $('#sendButton')
-const inputText = $('#inputText')
+const $ = document.querySelector.bind(document);
+const sendButton = $("#sendButton");
+const inputText = $("#inputText");
+const messageList = $(".messages");
 
-function onSendButtonClick(){
-    alert("Clicou no botão!")
+function onSendButtonClick() {
+  sendMessage();
 }
 
-function onEnterInputText(){
-    alert("Apertou enter")
+function onEnterInputText() {
+  sendMessage();
 }
 
-sendButton.addEventListener('click',onSendButtonClick)
-inputText.addEventListener('keydown',(event)=>{
+sendButton.addEventListener("click", onSendButtonClick);
+inputText.addEventListener("keydown", (event) => {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    onEnterInputText();
+  }
+});
 
-        if(event.keyCode == 13){
-            event.preventDefault()
-            onEnterInputText()
-        }
+function sendMessage() {
+  const text = inputText.value;
 
-})
+  if (text) {
+    createMessage({ myMessage: true, text });
+    inputText.value = "";
+  }
+
+  inputText.focus();
+}
+
+function createMessage({ myMessage, text }) {
+  const messageItem = document.createElement("li");
+  messageItem.classList.add(myMessage ? "myMessage" : "botMessage");
+  messageItem.textContent = text;
+  addNewMessage(messageItem);
+}
+
+function addNewMessage(message) {
+  messageList.appendChild(message);
+  scrollToBottom();
+}
+
+function scrollToBottom() {
+  messageList.scrollTo(0, messageList.scrollHeight);
+}
