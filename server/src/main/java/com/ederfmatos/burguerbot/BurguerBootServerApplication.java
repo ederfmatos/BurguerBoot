3078579e2530b476949221714ae1376d7b51e5bc
ederfmatos <ederfmatos@gmail.com>
2020-09-05 12:27:52 -0300

    Adicionado Spring ao Server

---
+++ server/src/main/java/com/ederfmatos/burguerbot/BurguerBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.ederfmatos.burguerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurguerBootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurguerBootServerApplication.class, args);
+    }
+
+}
