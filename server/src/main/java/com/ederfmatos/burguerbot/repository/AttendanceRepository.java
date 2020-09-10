8dcbf52e5d067555b7f8d4df1325d0462b0f53ac
ederfmatos <ederfmatos@gmail.com>
2020-09-09 23:06:57 -0300

    Criado scheduler para finalização de atendimentos

--- server/src/main/java/com/ederfmatos/burguerbot/repository/AttendanceRepository.java
+++ server/src/main/java/com/ederfmatos/burguerbot/repository/AttendanceRepository.java
@@ -1,10 +1,15 @@
 package com.ederfmatos.burguerbot.repository;
 
 import com.ederfmatos.burguerbot.model.Attendance;
+import com.ederfmatos.burguerbot.model.options.Option;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AttendanceRepository extends MongoRepository<Attendance, String> {
+
+    List<Attendance> findByFinishedAtNull();
 
 }
--- server/src/main/java/com/ederfmatos/burguerbot/repository/OptionRepository.java
+++ server/src/main/java/com/ederfmatos/burguerbot/repository/OptionRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OptionRepository extends MongoRepository<Option, String> {
+
 }
