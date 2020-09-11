f85da43cd9b284aeb0b87ac2c1d139021dd40fd9
ederfmatos <ederfmatos@gmail.com>
2020-09-11 17:52:02 -0300

    Adicionado estado para atendimento

---
+++ server/src/main/java/com/ederfmatos/burguerbot/model/enumeration/AttendanceStateEnum.java
@@ -0,0 +1,12 @@
+package com.ederfmatos.burguerbot.model.enumeration;
+
+public enum AttendanceStateEnum {
+
+    OPENED,
+    PREPARING,
+    FINISHING,
+    WAITING_DELIVERY,
+    DELIVERED,
+    CANCELED;
+
+}
