27eabdc21074c3852006bd01f787656c01c6cf84
ederfmatos <ederfmatos@gmail.com>
2020-09-11 19:54:43 -0300

    ALterado regra de cancelamento de atendimento para AttendanceService

--- server/src/main/java/com/ederfmatos/burguerbot/schedule/BotSchedule.java
+++ server/src/main/java/com/ederfmatos/burguerbot/schedule/BotSchedule.java
@@ -2,18 +2,11 @@ package com.ederfmatos.burguerbot.schedule;
 
 import com.ederfmatos.burguerbot.handler.BurgerBotSocketHandler;
 import com.ederfmatos.burguerbot.model.Attendance;
-import com.ederfmatos.burguerbot.model.enumeration.AttendanceStateEnum;
-import com.ederfmatos.burguerbot.repository.AttendanceRepository;
+import com.ederfmatos.burguerbot.service.AttendanceService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @EnableScheduling
 @Component
@@ -22,38 +15,20 @@ public class BotSchedule {
 
     private static final String CRON_LATE_BOT = "0/30 * * * * *";
 
-    private final AttendanceRepository attendanceRepository;
+    private final AttendanceService attendanceService;
 
-    @Value("${bot.responseWaitLimitInMinutes}")
-    private int responseWaitLimitInMinutes;
-
-    public BotSchedule(AttendanceRepository attendanceRepository) {
-        this.attendanceRepository = attendanceRepository;
+    public BotSchedule(AttendanceService attendanceService) {
+        this.attendanceService = attendanceService;
     }
 
     @Scheduled(cron = CRON_LATE_BOT)
     public void logAttendances() {
-        List<Attendance> attendances = attendanceRepository.findByFinishedAtNull();
-
-        final Predicate<Attendance> exceededWaitingTime = attendance ->
-                AttendanceStateEnum.CANCELED != attendance.getState()
-                        && ((attendance.getMessages().isEmpty() && attendance.getCreatedAt().isBefore(LocalDateTime.now().minusMinutes(responseWaitLimitInMinutes)))
-                        || attendance.getMessages().get(attendance.getMessages().size() - 1).getDateTime().isBefore(LocalDateTime.now().minusMinutes(responseWaitLimitInMinutes)));
-
-        final List<Attendance> stoppedAttendances = attendances.stream()
-                .filter(exceededWaitingTime)
+        attendanceService
+                .findAttendancesToCancel()
+                .filter(attendance -> BurgerBotSocketHandler.send("Atendimento sendo finalizado por falta de interação, todos os pedidos feitos serão desconsiderados!"))
                 .peek(attendance -> log.warn("Atendimento [{}] sendo cancelado por falta de interação", attendance))
                 .peek(Attendance::cancel)
-                .collect(Collectors.toList());
-
-        if (!stoppedAttendances.isEmpty()) {
-            log.warn("{} atendimento(s) estão sendo cancelado(s) por falta de interação", stoppedAttendances.size());
-            stoppedAttendances.forEach(attendance -> {
-                if (BurgerBotSocketHandler.send("Atendimento sendo finalizado por falta de interação, todos os pedidos feitos serão desconsiderados!")) {
-                    attendanceRepository.save(attendance);
-                }
-            });
-        }
+                .forEach(attendanceService::save);
     }
 
 }
