83e072eae02a4f677a5de989be5cb299a69a5844
ederfmatos <ederfmatos@gmail.com>
2020-09-14 20:40:08 -0300

    Adicionado enum de emojis

--- server/src/main/java/com/ederfmatos/burguerbot/exception/InvalidOptionException.java
+++ server/src/main/java/com/ederfmatos/burguerbot/exception/InvalidOptionException.java
@@ -1,11 +1,12 @@
 package com.ederfmatos.burguerbot.exception;
 
+import com.ederfmatos.burguerbot.model.enumeration.EmojiEnum;
 import com.ederfmatos.burguerbot.utils.BurgerBotMessages;
 
 public class InvalidOptionException extends BurgerBotException {
 
     public InvalidOptionException() {
-        super(BurgerBotMessages.from("errors.invalid_option").string());
+        super(EmojiEnum.CRY + " " + BurgerBotMessages.from("errors.invalid_option").string());
     }
 
     public InvalidOptionException(String message) {
