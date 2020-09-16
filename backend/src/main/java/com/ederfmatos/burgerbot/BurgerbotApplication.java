541aa8e344be7570c273d89dc5b07198c7c42afd
ederfmatos <ederfmatos@gmail.com>
2020-09-16 13:58:53 -0300

    Criado estrutura básica do backend

---
+++ backend/src/main/java/com/ederfmatos/burgerbot/BurgerbotApplication.java
@@ -0,0 +1,13 @@
+package com.ederfmatos.burgerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerbotApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ederfmatos/burgerbot/BurgerbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederfmatos.burgerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
